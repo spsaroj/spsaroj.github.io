b44599d193912d7082ddf2ad35df2cc9602b7b2e
Saroj Paudel <sarojpaudel@Sarojs-MacBook-Pro.local>
2026-01-23 22:14:32 +0545

    updated gmail contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
       </div>
 
       <a
-        href="mailto:sp@sarojpaudel.com.np"
+        href="mailto:paudelsaroj553@gmail.com"
         className="group relative inline-flex items-center justify-center px-8 py-3 font-bold text-white transition-all duration-200 bg-neutral-900 font-lg rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 hover:bg-neutral-800 hover:shadow-xl hover:shadow-lime-500/20"
       >
         Say Hello
