b028acb573916ca40f17325a9e97b387f07fe576
Saroj Paudel <sarojpaudel@Sarojs-MacBook-Pro.local>
2026-02-14 18:43:06 +0545

    github pages issue resolved -2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/spsaroj.github.io/',
   plugins: [react(), tailwindcss()],
+  base: '/',
 })
